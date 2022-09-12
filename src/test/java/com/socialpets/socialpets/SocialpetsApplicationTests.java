55bccb22a12800fd7f56795a9ba7dd7a72988f6b
Nestor Villafañe Mass <mass.nestor@gmail.com>
2022-09-12 18:56:00 -0500

    Feature: se agrega Spring boot

---
+++ src/test/java/com/socialpets/socialpets/SocialpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialpets.socialpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
